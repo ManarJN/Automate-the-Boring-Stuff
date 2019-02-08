739d42f8a72eb725d468619f4a51e40124314adb
Manar Naboulsi <manar.naboulsi@gmail.com>
2019-02-07 21:37:27 -0800

    added part 2 of the ch.5.1 project

--- Ch5- Dictionaries and Structuring Data/5.1_fantasy_game_inventory.py
+++ Ch5- Dictionaries and Structuring Data/5.1_fantasy_game_inventory.py
@@ -3,10 +3,7 @@
 #fantasy game inventory
 
 
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-
-
-#defines the displayInventory function
+#defines function that lists items in an inventory and amount
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
@@ -17,5 +14,24 @@ def displayInventory(inventory):
 
 
 #calls displayInventory function on stuff
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 displayInventory(stuff)
-        
+
+
+#separates projects' outputs with an empty line
+print()
+
+
+#defines function that adds a list of items to an inventory
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return(inventory)
+
+#calls addtoInventory function on inv and dragonLoot
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
+    
