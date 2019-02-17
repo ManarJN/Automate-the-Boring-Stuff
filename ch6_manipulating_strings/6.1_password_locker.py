a7b153aadacdb5e1a54aa61b4b2c9f7e6f0640f3
Manar Naboulsi <manar.naboulsi@gmail.com>
2019-02-16 18:41:53 -0800

    updated 6.1 program

--- ch6_manipulating_strings/6.1_password_locker.py
+++ ch6_manipulating_strings/6.1_password_locker.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3.
+#! /usr/bin/env python3
 
 #automate the boring stuff
 #chapter 3- manipulating strings
@@ -10,14 +10,25 @@ PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
 
 import sys, pyperclip
 
-if len(sys.argv) < 2:
+#returns message if program is called upon without an arg
+#e.g. './6.1_password_locker.py'
+#sys.argv is the line entered in the terminal to run the program
+if len(sys.argv) < 2:   #2 refers to './6.1_password_locker.py(1) arg(2)', items separated by space
     print('Usage: python 6.1_password_locker.py [account] - copy account password')
     sys.exit()
 
 account = sys.argv[1]   #first command line arg is the account name
+                        #[1] refers to e.g. './6.1_password_locker.py[0] blog[1]'
 
+
+#if available, the password is copied to clipboard, otherwise it prints a message
 if account in PASSWORDS:
     pyperclip.copy(PASSWORDS[account])
     print('Password for ' + account + ' copied to clipboard.')
 else:
     print('There is no account named ' + account)
+
+
+
+#to run program from terminal, type './6.1_password_locker.py args'
+#e.g. './6.1_password_locker.py email' returns 'F7minlBDDuvMJuxESSKHFhTxFtjVB6'
