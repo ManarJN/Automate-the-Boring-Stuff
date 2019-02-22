e4abd8e3962671ff6e6d6a5a78bfc6b33eedb963
Manar Naboulsi <manar.naboulsi@gmail.com>
2019-02-22 00:27:37 -0800

    added project 7.2

---
+++ ch7_pattern_matching_with_regex/7.2_strong_password_detection.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+
+# Automate the Boring Stuff
+# Chapter 7- Pattern Matching with Regex
+# Strong Password Detection - Ensures a password is strong.
+
+import re
+
+# creates password regex
+pwLowCase = re.compile(r'[a-z]')   # checks for a lowercase letter
+pwUpCase = re.compile(r'[A-Z]')    # checks for an uppercase letter
+pwNum = re.compile(r'\d')          # checks for a number
+pwSpace = re.compile(r'\s')        # checks for spaces, tabs, or newline characters
+
+# defines pw strength checker function on user input
+def pwRegex():
+    strong = True
+    password = str(input('Please enter a password: '))
+    if len(password) < 8:
+        print('Your password must be at least 8 characters long.')
+        strong = False
+    if pwLowCase.search(password) == None:
+        print('Your password must contain a lowercase character.')
+        strong = False
+    if pwUpCase.search(password) == None:
+        print('Your password must contain an uppercase character.')
+        strong = False
+    if pwSpace.search(password) != None:
+        print('Your password cannot contain spaces, tabs, or newline characters.')
+        strong = False
+    if strong == True:   # if password passes above criteria
+        print('Your password is strong.')
+        
+
+# calls pw strength checker function
+pwRegex()
---
+++ ch7_pattern_matching_with_regex/7.3_regex_version_of_strip.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+# Automate the Boring Stuff
+# Chapter 7- Pattern Matching with Regex
+# Regex Version of strip() - Performs a strip() function using Regex.
+
+import re
+
